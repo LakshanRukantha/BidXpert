e5b4e682dea5607a8cea21e25fb27d003aa8bf47
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2024-10-31 03:24:28 +0530

    🌟 feat: develop delete bid feature

--- frontend/app/my-bids/page.tsx
+++ frontend/app/my-bids/page.tsx
@@ -10,6 +10,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { toast } from "@/hooks/use-toast";
 import axios from "axios";
 import { format } from "date-fns";
 import { LoaderIcon, Trash2 } from "lucide-react";
@@ -47,6 +48,30 @@ const MyBids = () => {
     fetchBids();
   }, [session.data?.user.id]);
 
+  const handleDeleteBid = async (bidId: number) => {
+    try {
+      await axios.delete(`https://localhost:7174/api/bid/${bidId}`).then(() => {
+        toast({
+          variant: "default",
+          title: "Success",
+          description: "Bid deleted successfully.",
+        });
+        setBids((prevBids) =>
+          prevBids.filter(
+            (bid: {
+              bid_id: number;
+              auction_title: string;
+              placed_on: string;
+              amount: number;
+            }) => bid.bid_id !== bidId
+          )
+        );
+      });
+    } catch (error) {
+      console.error("Error deleting bid:", error);
+    }
+  };
+
   return (
     <div className="relative flex items-center flex-col">
       {loading ? (
@@ -78,19 +103,22 @@ const MyBids = () => {
             {bids.map(
               (bid: {
                 bid_id: number;
-                name: string;
+                auction_title: string;
                 placed_on: string;
                 amount: number;
               }) => (
                 <TableRow key={bid.bid_id}>
                   <TableCell className="font-medium">{bid.bid_id}</TableCell>
-                  <TableCell>{bid.name || "No Name"}</TableCell>
+                  <TableCell>{bid.auction_title}</TableCell>
                   <TableCell>{bid.amount}</TableCell>
                   <TableCell>
                     {format(new Date(bid.placed_on), "dd/MM/yyyy")}
                   </TableCell>
                   <TableCell className="flex items-center gap-2 justify-end">
-                    <Button variant={"destructive"}>
+                    <Button
+                      variant={"destructive"}
+                      onClick={async () => await handleDeleteBid(bid.bid_id)}
+                    >
                       <Trash2 />
                       <span className="hidden lg:block">Delete</span>
                     </Button>
